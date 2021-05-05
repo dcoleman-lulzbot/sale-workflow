998f5151e3a4d145ebb5e420727f777f95cf6782
Dawson Coleman <dcoleman@lulzbot.com>
2021-05-05 11:02:56 -0500

    Calculate no-discount subtotal without taxes

--- sale_discount_display_amount/models/sale_order_line.py
+++ sale_discount_display_amount/models/sale_order_line.py
@@ -20,19 +20,11 @@ class SaleOrderLine(models.Model):
             line.price_total_no_discount = 0
             line.discount_total = 0
             if not line.discount:
-                line.price_total_no_discount = line.price_total
+                line.price_total_no_discount = line.price_subtotal
                 continue
-            price = line.price_unit
-            taxes = line.tax_id.compute_all(
-                price,
-                line.order_id.currency_id,
-                line.product_uom_qty,
-                product=line.product_id,
-                partner=line.order_id.partner_shipping_id,
-            )
 
-            price_total_no_discount = taxes["total_included"]
-            discount_total = price_total_no_discount - line.price_total
+            price_total_no_discount = line.price_unit * line.product_uom_qty
+            discount_total = price_total_no_discount - line.price_subtotal
 
             line.update(
                 {
